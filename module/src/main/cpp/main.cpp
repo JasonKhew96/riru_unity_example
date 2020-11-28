76839dc2301277b4622c9c65e3efb03c521ed54c
jasonkhew96 <29531167+JasonKhew96@users.noreply.github.com>
2020-11-28 22:33:52 +0800

    Update to Riru v22+

--- module/src/main/cpp/main.cpp
+++ module/src/main/cpp/main.cpp
@@ -3,22 +3,25 @@
 #include <pthread.h>
 #include <string.h>
 #include "hook_main.h"
+#include <riru.h>
+#include <malloc.h>
+#include <cstring>
 
 // You can remove functions you don't need
 
-extern "C" {
-#define EXPORT __attribute__((visibility("default"))) __attribute__((used))
-EXPORT void nativeForkAndSpecializePre(
+static void forkAndSpecializePre(
         JNIEnv *env, jclass clazz, jint *_uid, jint *gid, jintArray *gids, jint *runtimeFlags,
         jobjectArray *rlimits, jint *mountExternal, jstring *seInfo, jstring *niceName,
         jintArray *fdsToClose, jintArray *fdsToIgnore, jboolean *is_child_zygote,
-        jstring *instructionSet, jstring *appDataDir, jstring *packageName,
-        jobjectArray *packagesForUID, jstring *sandboxId) {
+        jstring *instructionSet, jstring *appDataDir, jboolean *isTopApp,
+        jobjectArray *pkgDataInfoList,
+        jobjectArray *whitelistedDataInfoList, jboolean *bindMountAppDataDirs,
+        jboolean *bindMountAppStorageDirs) {
     // packageName, packagesForUID, sandboxId are added from Android Q beta 2, removed from beta 5
-        enable_hack = isGame(env, *appDataDir);
+    enable_hack = isGame(env, *appDataDir);
 }
 
-EXPORT int nativeForkAndSpecializePost(JNIEnv *env, jclass clazz, jint res) {
+static void forkAndSpecializePost(JNIEnv *env, jclass clazz, jint res) {
     if (res == 0) {
         // in app process
         if (enable_hack) {
@@ -32,49 +35,127 @@ EXPORT int nativeForkAndSpecializePost(JNIEnv *env, jclass clazz, jint res) {
         // in zygote process, res is child pid
         // don't print log here, see https://github.com/RikkaApps/Riru/blob/77adfd6a4a6a81bfd20569c910bc4854f2f84f5e/riru-core/jni/main/jni_native_method.cpp#L55-L66
     }
-    return 0;
 }
 
-EXPORT __attribute__((visibility("default"))) void specializeAppProcessPre(
+static void specializeAppProcessPre(
         JNIEnv *env, jclass clazz, jint *_uid, jint *gid, jintArray *gids, jint *runtimeFlags,
         jobjectArray *rlimits, jint *mountExternal, jstring *seInfo, jstring *niceName,
         jboolean *startChildZygote, jstring *instructionSet, jstring *appDataDir,
-        jstring *packageName, jobjectArray *packagesForUID, jstring *sandboxId) {
+        jboolean *isTopApp, jobjectArray *pkgDataInfoList, jobjectArray *whitelistedDataInfoList,
+        jboolean *bindMountAppDataDirs, jboolean *bindMountAppStorageDirs) {
     // this is added from Android Q beta, but seems Google disabled this in following updates
 
     // packageName, packagesForUID, sandboxId are added from Android Q beta 2, removed from beta 5
 }
 
-EXPORT __attribute__((visibility("default"))) int specializeAppProcessPost(
+static void specializeAppProcessPost(
         JNIEnv *env, jclass clazz) {
     // this is added from Android Q beta, but seems Google disabled this in following updates
-    return 0;
 }
 
-EXPORT void nativeForkSystemServerPre(
+static void forkSystemServerPre(
         JNIEnv *env, jclass clazz, uid_t *uid, gid_t *gid, jintArray *gids, jint *runtimeFlags,
         jobjectArray *rlimits, jlong *permittedCapabilities, jlong *effectiveCapabilities) {
 
 }
 
-EXPORT int nativeForkSystemServerPost(JNIEnv *env, jclass clazz, jint res) {
+static void forkSystemServerPost(JNIEnv *env, jclass clazz, jint res) {
     if (res == 0) {
         // in system server process
     } else {
         // in zygote process, res is child pid
         // don't print log here, see https://github.com/RikkaApps/Riru/blob/77adfd6a4a6a81bfd20569c910bc4854f2f84f5e/riru-core/jni/main/jni_native_method.cpp#L55-L66
     }
-    return 0;
 }
 
-EXPORT int shouldSkipUid(int uid) {
+static int shouldSkipUid(int uid) {
     // by default, Riru only call module functions in "normal app processes" (10000 <= uid % 100000 <= 19999)
     // false = don't skip
     return false;
 }
 
-EXPORT void onModuleLoaded() {
+static void onModuleLoaded() {
     // called when the shared library of Riru core is loaded
 }
+
+extern "C" {
+
+int riru_api_version;
+RiruApiV9 *riru_api_v9;
+
+/*
+ * Init will be called three times.
+ *
+ * The first time:
+ *   Returns the highest version number supported by both Riru and the module.
+ *
+ *   arg: (int *) Riru's API version
+ *   returns: (int *) the highest possible API version
+ *
+ * The second time:
+ *   Returns the RiruModuleX struct created by the module.
+ *   (X is the return of the first call)
+ *
+ *   arg: (RiruApiVX *) RiruApi strcut, this pointer can be saved for further use
+ *   returns: (RiruModuleX *) RiruModule strcut
+ *
+ * The second time:
+ *   Let the module to cleanup (such as RiruModuleX struct created before).
+ *
+ *   arg: null
+ *   returns: (ignored)
+ *
+ */
+void *init(void *arg) {
+    static int step = 0;
+    step += 1;
+
+    static void *_module;
+
+    switch (step) {
+        case 1: {
+            auto core_max_api_version = *(int *) arg;
+            riru_api_version =
+                    core_max_api_version <= RIRU_MODULE_API_VERSION ? core_max_api_version
+                                                                    : RIRU_MODULE_API_VERSION;
+            return &riru_api_version;
+        }
+        case 2: {
+            switch (riru_api_version) {
+                case 9: {
+                    riru_api_v9 = (RiruApiV9 *) arg;
+
+                    auto module = (RiruModuleInfoV9 *) malloc(sizeof(RiruModuleInfoV9));
+                    memset(module, 0, sizeof(RiruModuleInfoV9));
+                    _module = module;
+
+                    module->supportHide = true;
+
+                    module->version = RIRU_MODULE_VERSION;
+                    module->versionName = RIRU_MODULE_VERSION_NAME;
+                    module->onModuleLoaded = onModuleLoaded;
+                    module->shouldSkipUid = shouldSkipUid;
+                    module->forkAndSpecializePre = forkAndSpecializePre;
+                    module->forkAndSpecializePost = forkAndSpecializePost;
+                    module->specializeAppProcessPre = specializeAppProcessPre;
+                    module->specializeAppProcessPost = specializeAppProcessPost;
+                    module->forkSystemServerPre = forkSystemServerPre;
+                    module->forkSystemServerPost = forkSystemServerPost;
+                    return module;
+                }
+                default: {
+                    return nullptr;
+                }
+            }
+        }
+        case 3: {
+            free(_module);
+            return nullptr;
+        }
+        default: {
+            return nullptr;
+        }
+    }
 }
 
+}
--- template/magisk_module/riru.sh
+++ template/magisk_module/riru.sh
@@ -1,8 +1,8 @@
 #!/sbin/sh
-RIRU_PATH="/data/misc/riru"
+RIRU_PATH="/data/adb/riru"
 RIRU_MODULE_ID="%%%RIRU_MODULE_ID%%%"
 RIRU_MODULE_PATH="$RIRU_PATH/modules/$RIRU_MODULE_ID"
-RIRU_MIN_API_VERSION=%%%RIRU_MIN_API_VERSION%%%
+RIRU_SECONTEXT="u:object_r:magisk_file:s0"
 
 abort_clean() {
   rm -rf "$MODPATH"
@@ -10,19 +10,22 @@ abort_clean() {
 }
 
 check_riru_version() {
+  RIRU_MIN_API_VERSION=%%%RIRU_MIN_API_VERSION%%%
+  RIRU_MIN_VERSION_NAME="%%%RIRU_MIN_VERSION_NAME%%%"
+
   ui_print "check_riru_version"
   if [ ! -f "$RIRU_PATH/api_version" ]; then
-    ui_print    "*********************************************************"
-    ui_print    "! Riru is not installed"
-    ui_print    "! Please download 'Riru - Core' from 'Magisk Manager' or https://github.com/RikkaApps/Riru/releases"
+    ui_print "*********************************************************"
+    ui_print "! Riru $RIRU_MIN_VERSION_NAME or above is required"
+    ui_print "! Please download 'Riru - Core' from 'Magisk Manager' or https://github.com/RikkaApps/Riru/releases"
     abort_clean "*********************************************************"
   fi
   RIRU_API_VERSION=$(cat "$RIRU_PATH/api_version")
   ui_print "- Riru API version: $RIRU_API_VERSION"
   if [ "$RIRU_API_VERSION" -lt $RIRU_MIN_API_VERSION ]; then
-    ui_print    "*********************************************************"
-    ui_print    "! The latest version of Riru is required"
-    ui_print    "! Please download 'Riru - Core' from 'Magisk Manager' or https://github.com/RikkaApps/Riru/releases"
+    ui_print "*********************************************************"
+    ui_print "! The latest version of Riru is required"
+    ui_print "! Please download 'Riru - Core' from 'Magisk Manager' or https://github.com/RikkaApps/Riru/releases"
     abort_clean "*********************************************************"
   fi
 }
